dd04512b325a2800cc8ad92f57367dd1f9db996f
Đoàn Nguyễn Nhật Quang <46508313+JD981@users.noreply.github.com>
2020-07-12 22:06:44 +0700

    Update snake.cpp
    finish drawSnake and moveSnake

--- snake.cpp
+++ snake.cpp
@@ -40,7 +40,47 @@ void initGame()
 
 void drawSnake()
 {
-	// Ve ran
+	system("cls");
+	for (int i = 0; i < width + 2; i++) {
+		cout << "#";
+	}
+	cout << endl;
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0)
+				cout << "#";
+
+			if (i == snake[0].y && j == snake[0].x)
+				cout << "O";
+
+			else if (i == fruitY && j == fruitX)
+				cout << "F";
+
+			else {
+				bool print = false;
+				for (int k = 1; k < snakeLength; k++) {
+					if (snake[k].x == j && snake[k].y == i) {
+						cout << "o";
+						print = true;
+					}
+				}
+				if (!print)
+					cout << " ";
+			}
+
+			if (j == width - 1)
+				cout << "#";
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < width + 2; i++) {
+		cout << "#";
+	}
+	cout << endl;
+	cout << "Score: " << score << endl;
 }
 
 void changeDir()
@@ -69,7 +109,47 @@ void changeDir()
 
 void moveSnake()
 {
-	// Ran di
+	for (int i = 0; i < snakeLength; i++) {
+			if (i == 0) {
+				snake[0].x0 = snake[0].x; snake[0].y0 = snake[0].y;
+			}
+			else
+			{
+				snake[i].x0 = snake[i].x; snake[i].y0 = snake[i].y;
+				snake[i].x = snake[i - 1].x0; snake[i].y = snake[i - 1].y0;
+			}
+	}
+
+	switch (dir)
+	{
+	case LEFT:
+		snake[0].x--;
+		break;
+	case RIGHT:
+		snake[0].x++;
+		break;
+	case UP:
+		snake[0].y--;
+		break;
+	case DOWN:
+		snake[0].y++;
+		break;
+	}
+	if (snake[0].x > width || snake[0].x < 0 || snake[0].y > height || snake[0].y < 0) {
+		gameover = true;
+		initGame();
+	}
+
+	for (int i = 1; i < snakeLength; i++) {
+		if (snake[i].x == snake[0].x && snake[i].y == snake[0].y)
+			gameover = true;
+	}
+
+	if (snake[0].x == fruitX && snake[0].y == fruitY) {
+		score++;
+		snakeLength++;
+		spawnFruit();
+	}
 }
 
 int main()
